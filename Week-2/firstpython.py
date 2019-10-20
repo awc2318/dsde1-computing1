49d0baa0e20cf7b40f5635d9142e5a6d5d68bcd1
Gus <awc2318@ic.ac.uk>
2019-10-20 19:01:48 +0100

    Adding File

---
+++ Week-2/firstpython.py
@@ -0,0 +1,22 @@
+#Python
+message = 'Person 1: Whats the password?'
+password = "Person 2: He's not dead he's pining for the fjords"
+print(message)
+print(password)
+
+import math
+x = 3
+y = 2 + math.sin(x)
+print(y)
+
+x = 5
+z = math.log(x-3) + 2*(x+4)
+print(z)
+
+print('So what you doing here?')
+Talk = input(' ')
+print(Talk + ' really? You Sure?')
+
+Print('Say a number between 1 - 10')
+guess = input('')
+guess = int(guess)
